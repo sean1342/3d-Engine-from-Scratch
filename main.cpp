268deae369750592d4cd2e91bea3ebddceba75c9
Sean Thompson <98042267+sean1342@users.noreply.github.com>
2023-02-11 10:43:53 -0500

    working good with glm and optimized MatrixMultiplyVector  function that uses glm

--- main.cpp
+++ main.cpp
@@ -45,12 +45,9 @@ struct triangle
 
 glm::vec4 MultiplyMatrixVector(glm::vec4 &i, glm::mat4 &m)
 {
-	glm::vec4 o;
-	o.x = i.x * m[0][0] + i.y * m[1][0] + i.z * m[2][0] + m[3][0];
-	o.y = i.x * m[0][1] + i.y * m[1][1] + i.z * m[2][1] + m[3][1];
-	o.z = i.x * m[0][2] + i.y * m[1][2] + i.z * m[2][2] + m[3][2];
-	float w = i.x * m[0][3] + i.y * m[1][3] + i.z * m[2][3] + m[3][3];
+	glm::vec4 o = m * i;
 
+	float w = i.x * m[0][3] + i.y * m[1][3] + i.z * m[2][3] + m[3][3];
 	if (w != 0.0f)
 		o.x /= w; o.y /= w; o.z /= w;
 
@@ -180,10 +177,6 @@ int main(int argc, char *argv[])
 
 				// how similar is normal to light direction
 				tri_projected.dp = normal.x * light_direction.x + normal.y * light_direction.y + normal.z * light_direction.z;
-
-				// tri_projected.p[0] = mat_proj * tri_translated.p[0];
-				// tri_projected.p[1] = mat_proj * tri_translated.p[1];
-				// tri_projected.p[2] = mat_proj * tri_translated.p[2];
 
 				tri_projected.p[0] = MultiplyMatrixVector(tri_translated.p[0], mat_proj);
 				tri_projected.p[1] = MultiplyMatrixVector(tri_translated.p[1], mat_proj);
